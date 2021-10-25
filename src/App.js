c53369c8b8f367ebcd33d802a9440434c6b56ccb
WebDev-Chad <chadwin.allotey@gmail.com>
2021-10-25 02:22:45 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Navbar from './Component/Navbar';
+import Home from './Component/Home';
+import Characters from './Component/Characters';
+// import React, { useState } from "react";
+// import Axios from "axios";
 
-function App() {
+const App = () => {
+
+// const url ='https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters';
+
+
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <Navbar />
+     <Home />
+     <Characters />
+   </>
   );
 }
+
 
 export default App;
